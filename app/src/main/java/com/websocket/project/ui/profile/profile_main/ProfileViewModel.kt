b04936b0fbe570d91eb350681ef5fa23d15d7275
evgenii fadin <chester0098@gmail.com>
2022-01-18 15:04:18 +0300

    change method hideEmail()

--- app/src/main/java/com/websocket/project/ui/profile/profile_main/ProfileViewModel.kt
+++ app/src/main/java/com/websocket/project/ui/profile/profile_main/ProfileViewModel.kt
@@ -8,21 +8,10 @@ import javax.inject.Inject
 @HiltViewModel
 class ProfileViewModel @Inject constructor() : ViewModel() {
 
-    //bad decision sorry, later I will rewrite normally
     fun hideEmail(email: String): String {
-        val stringBuilder = StringBuilder()
-        var splitEmail = email.split("@")
-        stringBuilder.append(email[0])
-        for (i in 1..splitEmail[0].length) {
-            stringBuilder.append("*")
-        }
-        stringBuilder.append("@")
-        splitEmail = splitEmail[1].split(".")
-        splitEmail[0].forEach { _ ->
-            stringBuilder.append("*")
-        }
-        stringBuilder.append(".").append(splitEmail[1])
-
-        return stringBuilder.toString()
+        return email.replace(
+            Regex("(?<=.)[^@\\n](?=[^@\\n]*?@)|(?:(?<=@)|(?!^)\\G(?=[^@\\n]*\$)).(?=.*\\.)"),
+            "*"
+        )
     }
 }
