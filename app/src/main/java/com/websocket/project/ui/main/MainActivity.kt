d110bcce84859f8876e5aa52dc8b0e2eec51809f
Roman Mirin <r.mirin@zorra.com>
2022-01-19 10:51:55 +0300

    bottom nav view and menu

--- app/src/main/java/com/websocket/project/ui/main/MainActivity.kt
+++ app/src/main/java/com/websocket/project/ui/main/MainActivity.kt
@@ -5,11 +5,14 @@ import com.websocket.project.R
 import com.websocket.project.ui.custom.rsi_view.RsiView
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.websocket.project.ui.base.setupWithNavController
 import com.websocket.project.ui.custom.rsi_view.RsiIndicator
-
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,24 @@ class MainActivity : AppCompatActivity() {
 //                .commitNow()
 //        }
         //Example of usage of RsiView
-        val rsiView = findViewById<RsiView>(R.id.main_rsi_view)
-        rsiView.activateIndicator(RsiIndicator.BUY)
+//        val rsiView = findViewById<RsiView>(R.id.main_rsi_view)
+//        rsiView.activateIndicator(RsiIndicator.BUY)
+    }
+
+    private fun initBottomNavigation() {
+
+        bottomNavigationView = findViewById(R.id.main_bottom_nav_view)
+
+        bottomNavigationView.setupWithNavController(
+            navGraphIds = listOf(
+                R.navigation.nav_graph_main,
+                R.navigation.nav_graph_markets,
+                R.navigation.nav_graph_trade,
+                R.navigation.nav_graph_wallet
+            ),
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.main_nav_host_container,
+            intent = intent
+        )
     }
 }
